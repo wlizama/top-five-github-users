5b876e7581a07ed6fa6a00ccf3ae088727345276
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2022-03-16 23:18:06 -0500

    Home View

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import {
+    Link,
+} from 'react-router-dom'
+import './Home.scss'
 
 const Home = () => {
+
+    const usernames = ["GrahamCampbell","fabpot","weierophinney","rkh","josh"];
+
     return (
-        <div>Home</div>
+        <section className='Home'>
+            <h2 className='Home__title'>Top 5 GitHub Users</h2>
+            <h5 className='Home__subtitle'>Tap the username to see more information</h5>
+            <ul className='Home__list'>
+                {usernames.map(username => (
+                <li 
+                    key={username}
+                    className='Home__list-item'
+                >
+                    <Link to={`/person/${username}`}>{username}</Link>
+                </li>
+                ))}
+            </ul>
+        </section>
     )
 }
 
